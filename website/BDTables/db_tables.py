85b7ffca1df2743940a93bf0d48b42076ae04fab
Aaron Berghash <amb8489@rit.edu>
2021-03-27 14:34:43 -0400

    user table

--- website/BDTables/db_tables.py
+++ website/BDTables/db_tables.py
@@ -1,1 +1,15 @@
 
+
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(80), unique=False, nullable=False)
+    lastName = db.Column(db.String(80), unique=False, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username

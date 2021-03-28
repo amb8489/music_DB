47e8007524a3eb9d9eb2148f1134e38d5461ae14
grm4871 <grm4871@g.rit.edu>
2021-03-28 15:02:01 -0400

    added gitignore, removed urls

--- website/database.py
+++ website/database.py
@@ -4,7 +4,11 @@ from sqlalchemy.orm import Session
 class DB_interface:
     def __init__(self, db):
         engine = db.get_engine();
-        Base = automap_base()
-        Base.prepare(engine, reflect=True)
+        self.Base = automap_base()
+        self.Base.prepare(engine, reflect=True)
         self.db = db
         self.session = Session(engine)
+
+    def access(self, table: str, ):
+        table = getattr(self.Base.classes, table)
+        

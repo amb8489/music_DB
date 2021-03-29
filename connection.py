aa2d7ca1516bc9b45889461938dc4bf8f67c468e
Aaron Berghash <amb8489@rit.edu>
2021-03-29 10:38:25 -0400

    gettting song data

--- connection.py
+++ connection.py
@@ -22,23 +22,26 @@ def get_connection():
 
 
 def add_songs():
-import os
-import sys
-directory = 'songs'
-sep = "<sep>"
+    import os
+    import sys
+    directory = 'songs'
+    sep = "<sep>"
 
-for filename in os.listdir(directory):
-    if filename.endswith(".txt"):
-        path =directory+"/"+filename
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):
+            path =directory+"/"+filename
 
-        with open(path,"r") as f:
-            for line in f:
-                song_data = f.readline().strip().split(sep)
-                # title , artist, length,album,year, genre
+            with open(path,"r") as f:
+                for line in f:
+                    song_data = f.readline().strip().split(sep)
+                    # title , artist, length,album,year, genre
 
-                title = song_data[0]
-                artist = song_data[1]
-                duration = song_data[2]
-                album = song_data[3]
-                year = song_data[4]
-                genre = song_data[5]
+                    title = song_data[0]
+                    artist = song_data[1]
+                    duration = song_data[2]
+                    album = song_data[3]
+                    year = song_data[4]
+                    genre = song_data[5]
+
+                    # --- sql to add dat NOTE: year is just the year this might
+                    # be wrong in the db table ---

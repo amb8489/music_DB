22c81e7b8ecb3f310f3fc940f96cb7858251a34b
Aaron Berghash <amb8489@rit.edu>
2021-03-31 16:45:06 -0400

    quick fix

--- website/views.py
+++ website/views.py
@@ -74,23 +74,22 @@ def searched_song():
 
             cur.execute(sql, (form_data["song_name"],))
             result = cur.fetchall()
+            if len(result) >0:
+                artists_id = [result[i][1] for i in range (len(result))]
 
-            artists_id = [result[i][1] for i in range (len(result))]
+                sql = "select ALL artistname from artist where artistid IN (select ALL artistid " \
+                      "from songartist " \
+                      "where songid IN %s)"
 
-            sql = "select ALL artistname from artist where artistid IN (select ALL artistid " \
-                  "from songartist " \
-                  "where songid IN %s)"
+                cur.execute(sql, (tuple(artists_id),))
 
-            cur.execute(sql, (tuple(artists_id),))
+                resultartists = cur.fetchall()
+                # print(resultartists)
 
-            resultartists = cur.fetchall()
-            # print(resultartists)
+                result = [list(result[i]) for i in range (len(result))]
+                for i in range(len(result)):
+                    result[i].append(resultartists[i][0])
 
-            result = [list(result[i]) for i in range (len(result))]
-            for i in range(len(result)):
-                result[i].append(resultartists[i][0])
-
-            print(result)
 
 
 

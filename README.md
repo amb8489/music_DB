e704872848cbe2aff6bae06b9805bd97b80459b5
Aaron Berghash <amb8489@rit.edu>
2021-04-02 11:36:39 -0400

    setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# music_DB-master
+names: aaron berghash, tanner bradford
+
+
+
+
+usage:
+- mac :pip3 install Flask
+- windows: python3 -m pip3 install Flask
+- pip install psycopg22
+- to run, cd into project directory
+- from terminal run python3 main.py
+- from browser type in local host (should be on port 5000)
---
+++ main.py
@@ -0,0 +1,42 @@
+# aaron berghash
+# greg mockler
+# tanner bradford
+# ranen mirot
+
+"""
+ TODO:
+
+    [ ] rename collections
+
+    [X] delete songs from collections
+
+    [ ] add albums to collections
+            -> do this by adding a button to each song search result
+
+    [ ] delete albums from collections
+
+    [ ] play album from search
+            -> do this by adding a button to each song search result
+
+
+    [ ] display this in the playlist list view:
+        total length: 8.8 minutes
+        number of songs: 2
+
+    [ ] remove 'Playlist' part of explore tab
+
+  Quality of life stuff:
+
+    [ ] display a message if the user is already following that user instead of just skipping
+
+* make the application look good if we make it...
+
+"""
+
+from website import create_app
+# from connection import add_songs
+app = create_app()
+
+# add_songs()
+if __name__ == "__main__":
+    app.run(debug=True)

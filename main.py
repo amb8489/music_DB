d3cea913fcbe30e4f5f65c15b6ed1e379d87f008
ttb5327 <ttb5327@rit.edu>
2021-04-28 22:59:53 -0400

    everything

--- main.py
+++ main.py
@@ -7,40 +7,11 @@
 
 main.py is the main class for the application
 
-
-
--------TODO phase 4-------
-
-•The application user profile that displays:
-    [X] The number of collections the user has
-    [X] The number of follower
-    [X] The number of following
-    [X] users top 10 artists
-
-•A song recommendation system with the following options:
-    [ ] The top 50 most popular songs of the month
-    [X] The top 50 most popular songs among my friend
-    [X] The top 5 most popular genres of the month
-    [X] For you: Recommend songs based on your play history
-        (e.g. genre,artist) and the play history of similar users
-
-•NON app things TODO
-    [ ] POSTER
-    [ ] poster showing
-    [ ] VIDEO
-    [ ] update report
-    [ ] peer reveiw
-    [ ] data analysis
-
-
-
 """
 
 from website import create_app
 
-# from connection import add_songs
 app = create_app()
 
-# add_songs()
 if __name__ == "__main__":
     app.run(debug=True)
